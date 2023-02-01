c678258bc9681701b904db8833a79f0d41a2fc6a
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2023-02-01 16:32:06 -0500

    Spring Initializr files

---
+++ java-news-java-api/src/main/java/com/javanews/JavaNewsJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.javanews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNewsJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaNewsJavaApiApplication.class, args);
+	}
+
+}
---
+++ java-news-java-api/src/test/java/com/javanews/JavaNewsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNewsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
