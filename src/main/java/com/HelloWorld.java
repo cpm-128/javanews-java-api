352349d8c453392b9eb3ea6995b9f38674952742
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2023-02-02 12:25:22 -0500

    updated to Java 17

---
+++ src/main/java/com/HelloWorld.java
@@ -0,0 +1,8 @@
+package com;
+
+//Name of class MUST correspond to file name
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
