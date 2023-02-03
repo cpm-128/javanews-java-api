fe2997587d87e5c04eeedfa5d3b3ef4e38d8b835
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2023-02-03 12:52:20 -0500

    document data types

--- src/main/java/com/javanews/VariableTypes.java
+++ src/main/java/com/javanews/VariableTypes.java
@@ -2,6 +2,10 @@ package com.javanews;
 
 public class VariableTypes {
     public static void main(String[] args) {
-
+        String name = "Cleopatra";
+        int age = 2016 + 69;
+        double price = 29.95;
+        boolean truth = true;
+        boolean falseness = !truth;
     }
 }
