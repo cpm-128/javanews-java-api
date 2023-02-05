70a513ed03362eed37f036172669abd208fcc403
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2023-02-05 13:47:34 -0500

    sum output in main() method

---
+++ src/main/java/com/javanews/VariablesPractice.java
@@ -0,0 +1,9 @@
+package com.javanews;
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int whole = 7;
+        double decimal = 8.95;
+        double sum = whole + decimal;
+        System.out.println("The sum is " + sum);
+    }
+}
